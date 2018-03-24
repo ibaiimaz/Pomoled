dcddcd50dbc86b73a2b7cb07ac6b1015062ddd63
ibai <ibai@diversius.com>
2018-03-24 17:50:49 +0100

    Implementationadd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+import { Socket } from 'ng-socket-io';
+
+import { PomodoroService } from './pomodoro.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  statusGonzalo = 3;
+  statusIbai = 3;
+
+  constructor(
+    private pomodoroService: PomodoroService,
+    private socket: Socket
+  ) {}
+
+  ngOnInit() {
+    this.initSockets();
+  }
+
+  async onStartGonzalo() {
+    await this.pomodoroService.start('gonzalo').toPromise();
+    // this.statusGonzalo = 1;
+  }
+
+  async onStartIbai() {
+    await this.pomodoroService.start('ibai').toPromise();
+    // this.statusIbai = 1;
+  }
+
+  private initSockets() {
+
+    this.socket
+      .fromEvent('pomodoro')
+      .pipe(map( (data: any) => this.setStatus(data)))
+      .subscribe();
+
+    this.socket
+      .fromEvent('pomodoro:users')
+      .pipe(map( (data: any) => this.setInitialStatus(data)))
+      .subscribe();
+  }
+
+  private setInitialStatus(data) {
+    console.log(data);
+    this.statusGonzalo = data.gonzalo;
+
+    this.statusIbai = data.ibai;
+  }
+
+  private setStatus(data) {
+    console.log(data);
+    if (data.user === 'gonzalo') {
+      this.statusGonzalo = data.status;
+    }
+    if (data.user === 'ibai') {
+      this.statusIbai = data.status;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { SocketIoModule, SocketIoConfig } from 'ng-socket-io';
 
 import { AppComponent } from './app.component';
+import { SemaphoreComponent } from './semaphore/semaphore.component';
+import { PomodoroService } from './pomodoro.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
+const config: SocketIoConfig = { url: 'http://192.168.1.42:3000', options: {} };
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SemaphoreComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    SocketIoModule.forRoot(config)
   ],
-  providers: [],
+  providers: [
+    PomodoroService,
+    HttpClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
